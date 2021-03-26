b317b35f998bc2c71e5f7f23fbdad4a2f8f7c69c
Leah Miller <leahmiller1969@gmail.com>
2021-03-26 16:12:59 -0700

    making API call

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const https = require("https"); // native way to make API requests
 
 const app = express();
 
 app.get("/", function(req, res){
+  const url = "https://api.weatherapi.com/v1/current.json?key=14b16247a3ae4236b0e220244212603&q=89135&aqi=yes";
+  https.get(url, function(response){
+    console.log(response);
+  })
   res.send("Server up and running")
 })
 
