537fa52c83135d29fdf4744ec02f7e3c8422e2a0
Leah Miller <leahmiller1969@gmail.com>
2021-03-27 12:21:24 -0700

    working weather app

--- app.js
+++ app.js
@@ -4,20 +4,29 @@ const https = require("https"); // native way to make API requests
 const app = express();
 
 app.get("/", function(req, res){
-  const url = "https://api.weatherapi.com/v1/current.json?key=14b16247a3ae4236b0e220244212603&q=89135&aqi=yes";
+  const locationQuery = 89135;
+  const apiKey = "14b16247a3ae4236b0e220244212603";
+  const url = "https://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + locationQuery + "&aqi=yes";
   https.get(url, function(response){
-    console.log(response.statusCode);
+    
     response.on("data", function(data){  //search through the response
       const weatherData = JSON.parse(data); //returns data into a JSON object. opposite would be JSON.stringify() to compact an object
       const temp_f = weatherData.current.temp_f;
       const temp_c = weatherData.current.temp_c;
-      const wind_kph = weatherData.wind_kph;
-      const wind_mph = weatherData.wind_mph;
+      const wind_kph = weatherData.current.wind_kph;
+      const wind_mph = weatherData.current.wind_mph;
 
-
+      const icon =  weatherData.current.condition.icon;
+      
+      res.write("<p>Wind speed in KPH is " + wind_kph + ", which is " + wind_mph + " in MPH.</p>");
+      res.write("<p>The temperature in Celsius is "+ temp_c + ". Which is " + temp_f + " in Fahrenheit.</p>");
+      res.write("<img src='" + icon + "' />");
+      // can only have 1 'res.send' - use 'res.write' to write multiple
+      res.send();
     })  
   })
-  res.send("Server up and running")
+
+  // res.send("Server up and running")
 })
 
 
